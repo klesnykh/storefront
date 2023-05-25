01691ce1a4b043850a233f4ce77b00383bc470ef
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-05-25 14:41:06 -0700

    lab39 mostly done

--- src/store/cart.js
+++ src/store/cart.js
@@ -7,16 +7,23 @@ function cartReducer(state = initialState, action) {
   case 'ADD_TO_CART':
     //action.payload is a product
     console.log(action.payload);
+    let newList = [...state.list, action.payload];
+    localStorage.setItem('cart', JSON.stringify(newList));
     return {
-      list: [...state.list, action.payload]
+      list: newList,
     };
   case 'REMOVE_FROM_CART':
     //action.payload is a PRODUCT
     //find the product, remove 1 from it's stock
     let newCart = state.list.filter(product => product!==action.payload);
+    localStorage.setItem('cart', JSON.stringify(newCart));
     return {
       list: newCart,
     };
+  case 'LOAD_CART':
+    return{
+      list: action.payload,
+    }
   default: return state;
   }
 }
